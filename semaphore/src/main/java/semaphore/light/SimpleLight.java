5e83bd486993dc1bd756e88236c82b1280563c8c
Wenningkamp <gab.wenningkamp@gmail.com>
2025-04-08 22:05:24 -0300

    Aula03-Class

--- semaphore/src/main/java/semaphore/light/SimpleLight.java
+++ semaphore/src/main/java/semaphore/light/SimpleLight.java
@@ -1,5 +1,23 @@
 package semaphore.light;
 
-public class SimpleLight {
 
-}
+public class SimpleLight implements Light {
+	
+	private OnOff state = OnOff.OFF;
+	@Override
+	public void turnOn() {
+	this.state = OnOff.ON;
+	}
+	@Override
+	public void turnOff() {
+	this.state = OnOff.OFF;
+	}
+	@Override
+	public boolean isOn() {
+	return this.state == OnOff.ON;
+	}
+	@Override
+	public boolean isOff() {
+	return this.state == OnOff.OFF;
+	}
+	}
